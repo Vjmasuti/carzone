2e51acfbc4df6401fa284492cea6c9009851e388
Vjmasuti <vijaylaxmimasuti@gmail.com>
2020-10-17 14:54:16 +0530

    My first commit

---
+++ pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request,'pages/home.html', {'name':'pages/home.html'})
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
